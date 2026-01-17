8f24959fe85491bc5956d1b30dbe8b359113563a
Arpit Rajpurohit <164439921+arpitraj10@users.noreply.github.com>
2026-01-17 23:53:35 +0530

    Delete house_price_prediction_web directory

--- house_price_prediction_web/backend/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class HouseInput(BaseModel):
-    overall_qual: int
-    gr_liv_area: float
-    garage_cars: int
-    total_bsmt_sf: float
-    year_built: int
--- house_price_prediction_web/backend/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def confidence_interval(pred, std):
-    return {
-        "min": round(pred - 1.96 * std, 2),
-        "max": round(pred + 1.96 * std, 2)
-    }
