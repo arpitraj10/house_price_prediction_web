e4ea64c93e09b82fd59c4855e54f74f44b873fa1
Arpit <arpurohit0123@gmail.com>
2026-01-19 16:43:37 +0530

    Install recharts and fix FeatureChart build error

--- frontend/src/components/FeatureChart.jsx
+++ frontend/src/components/FeatureChart.jsx
@@ -1,5 +1,17 @@
-<BarChart data={data}>
-  <XAxis dataKey="feature" />
-  <YAxis />
-  <Bar dataKey="importance" />
-</BarChart>
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
+const FeatureChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data}>
+        <XAxis dataKey="feature" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="value" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default FeatureChart;
+
