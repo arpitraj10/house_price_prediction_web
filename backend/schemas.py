5c2bf3751cc3ca955334d4c27b30a4933d8f8689
Arpit <arpurohit0123@gmail.com>
2026-01-19 20:46:17 +0530

    Add full_bath to HouseInput schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel
 
 class HouseInput(BaseModel):
     overall_qual: int
-    gr_liv_area: float
+    gr_liv_area: int
     garage_cars: int
-    total_bsmt_sf: float
-    
+    total_bsmt_sf: int
+    full_bath: int
+    year_built: int
